a5b9650c58386f333426db047a38205c95744cab
HuzaifSheikh <77417963+HuzaifSheikh@users.noreply.github.com>
2021-03-05 15:24:19 -0500

    Fixed a lot of issues with buttons.

--- app/src/main/java/com/example/respiratorapp/FormActivity.java
+++ app/src/main/java/com/example/respiratorapp/FormActivity.java
@@ -2,13 +2,39 @@ package com.example.respiratorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class FormActivity extends AppCompatActivity {
+
+    private ImageView home_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //makes this activity full-screen (removes notification bar)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_form);
+
+        initListeners();
+
+    }
+    protected void initListeners() {
+        home_btn = (ImageView) findViewById(R.id.imageView28);
+        home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FormActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
