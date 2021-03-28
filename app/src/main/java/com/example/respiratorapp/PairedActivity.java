c144cf28f9e7440b120b96789b0f31eeaf85dc58
jake-chandler <jake.chandler@uga.edu>
2021-03-28 03:48:02 -0400

    Add rough draft of BleService. Minor details todo, but is no longer blocking.

--- app/src/main/java/com/example/respiratorapp/PairedActivity.java
+++ app/src/main/java/com/example/respiratorapp/PairedActivity.java
@@ -1,14 +1,22 @@
 package com.example.respiratorapp;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -16,6 +24,8 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class PairedActivity extends AppCompatActivity {
     private ImageView home_btn;
+    BleService svc;
+    Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,10 @@ public class PairedActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_paired);
+
+        Intent intent = new Intent (this, BleService.class);
+
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
 
         initListeners();
     }
@@ -40,4 +54,23 @@ public class PairedActivity extends AppCompatActivity {
             }
         });
     }
+
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.i("PAIRED", "Ble Service discovered.");
+            BleService.BleServiceBinder binder = (BleService.BleServiceBinder) service;
+            svc = binder.getService();
+            svc.notifyHR(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+            Log.i("PAIRED", "Notifying service...");
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i("PAIRED", "Service unbounded from this activity.");
+        }
+    };
 }
--- app/src/main/java/com/example/respiratorapp/PairingActivity.java
+++ app/src/main/java/com/example/respiratorapp/PairingActivity.java
@@ -5,19 +5,29 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
+import android.os.Parcelable;
 import android.view.Window;
 import android.view.WindowManager;
 import android.util.Log;
 
 import androidx.annotation.RequiresApi;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Represents the BLE pairing process. Attempts to pair to the MC.
@@ -25,76 +35,13 @@ import androidx.annotation.RequiresApi;
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class PairingActivity extends Activity {
 
-    /**
-     * Request code for enabling bluetooth services.
-     *
-     * @note check for consistency in the return method to verify successful enabling.
-     */
-    private static final int REQUEST_ENABLE_BT = 1;
-
-    /**
-     * The name of the device to pair with.
-     */
-    private static final String DEVICE_NAME = "uga_respiratory_mc";
-
-    /**
-     * Time to scan for device in milliseconds.
-     */
-    private static final long SCAN_TIME = 10000;
-
-    /**
-     * Maximum number of devices to scan for.
-     */
-    private static final short MAX_DEVICES = 20;
-
-    /**
-     * BLE Manager for adapter retrieval.
-     */
-    private final BluetoothManager bleManager = getSystemService(BluetoothManager.class);
-
-    /**
-     * BLE Adapter for determining if bluetooth is enabled.
-     */
-    private BluetoothAdapter bleAdapter = null;
-    private BluetoothLeScanner bleScanner;
-
-    private Handler handler;
-
-    /**
-     * GATT server connection to be exported to the 'TestingActivity' class.
-     */
-    private BluetoothGatt bleGatt;
-
-    /**
-     * True if the device is scanning, false otherwise.
-     */
-    private boolean scanning;
-
-    /**
-     * True if specified device is found, false otherwise.
-     */
-    private boolean deviceFound = false;
-
-    /**
-     * Scanned devices.
-     */
-    private final BluetoothDevice[] devices = new BluetoothDevice[MAX_DEVICES];
-
-    /**
-     * The desired device to connect to.
-     */
-    private BluetoothDevice bleDevice;
-
-    /**
-     * Used to iterate through the list of devices.
-     */
-    private short deviceListIterator;
-
+    BleService svc;
+    Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        handler = new Handler();
+
 
         //makes this activity full-screen (removes notification bar)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -103,142 +50,39 @@ public class PairingActivity extends Activity {
 
         setContentView(R.layout.activity_pairing);
 
-        // pair to the device
-        pair();
+        Intent intent = new Intent (this, BleService.class);
 
-    }
-
-    /**
-     * Sends GATT connection to 'TestingActivity' class.
-     *
-     */
-    private void postGattConnection() {
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        Log.i("PAIRING", "4");
 
 
-        // send GATT connection to
-
-    }
-
-    /**
-     * @brief Helper method used to connect to MC via BLE
-     */
-    private void pair() {
-        Log.i("PAIRING","Beginning connection process...");
-        deviceListIterator = 0;
-
-        if (bleManager != null) {
-            bleAdapter = bleManager.getAdapter();
-        } else {
-            Log.e("PAIRING", "Error. This device does not support BLE.");
-        }
-        if (bleAdapter != null && !bleAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            return;
-        } else {
-            Log.e("PAIRING", "Error. Please enable Bluetooth on this device.");
-        }
-
-        bleScanner = bleAdapter.getBluetoothLeScanner();
-
-        scan();
-
-    }
-
-    private void handleResults() {
-        boolean devicePaired;
-        if (deviceFound) {
-
-            // attempt to connect to GATT server.
-            try {
-                bleGatt = bleDevice.connectGatt(this, false, gattCallback);
-                Intent i = new Intent(this, PairingActivity.class);
-                bleGatt.write
-                bleGatt.getServices();
-
-                devicePaired = true;
-                Log.i("PAIRING", "Connected to GATT Server successfully.");
-            } catch (Exception e) {
-                devicePaired = false;
-                Log.e("PAIRING", "Failure to connect to GATT Server.",e);
-            }
-
-            if (devicePaired) {
-                postGattConnection();
-                changeScreens();
-            } else {
-                Log.e("PAIRING", "Failure to connect to GATT Server.");
-            }
-        } else {
-            // Device not found.
-            Log.i("PAIRING", "UGA Sensor BLE device was not found.");
-        }
-    }
-
-    /**
-     * Changes the activity.
-     */
-    private void changeScreens() {
-        Intent intent = new Intent(this, PairedActivity.class);
+        /**
+        Intent intent = new Intent(PairingActivity.this, PairedActivity.class);
         startActivity(intent);
+         */
+
+
     }
 
-    /**
-     * Scans for the BLE device for 10 seconds.
-     */
-    private void scan() {
-        Log.i("PAIRING", "Now scanning for BLE devices...");
-        if (bleScanner != null) {
-            if (!scanning) {
+    private ServiceConnection connection = new ServiceConnection() {
 
-                // Stops scanning after a pre-defined scan period.
-                handler.postDelayed(() -> {
-                    scanning = false;
-                    Log.i("PAIRING", "Terminating the scanning procedure.");
-                    bleScanner.stopScan(scanCallback);
-                    handleResults();
-                }, SCAN_TIME);
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.i("PAIRING", "Ble Service discovered.");
+            BleService.BleServiceBinder binder = (BleService.BleServiceBinder) service;
+            svc = binder.getService();
+            svc.setActivity(activity);
+            svc.setContext(getApplicationContext());
+            Intent intent = new Intent (activity, BleService.class);
+            startService(intent);
 
-                scanning = true;
-                bleScanner.startScan(scanCallback);
-
-            }
-        } else {
-            Log.e("PAIRING", "Error with BLE scanner.");
         }
-    }
 
-    /**
-     * Callback function that searches for the MC device upon a scan result.
-     */
-    private final ScanCallback scanCallback =
-            new ScanCallback() {
-
-                @Override
-                public void onScanResult(int callbackType, ScanResult result) {
-                    super.onScanResult(callbackType, result);
-                    Log.i("PAIRING", "A new BLE device has been scanned.");
-
-                    if (result.getDevice().getName().equals(DEVICE_NAME)) {
-                        bleDevice = result.getDevice();
-                        Log.i("PAIRING", "Device has been found.");
-                    }
-
-                }
-            };
-    private final BluetoothGattCallback gattCallback =
-            new BluetoothGattCallback() {
-                @Override
-                public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-                    super.onConnectionStateChange(gatt, status, newState);
-                }
-
-                @Override
-                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                    super.onServicesDiscovered(gatt, status);
-
-                }
-            };
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i("PAIRING", "Service unbounded from this activity.");
+        }
+    };
 
 
 }
